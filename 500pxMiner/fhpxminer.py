0792e0fae0fc9307153a02d550d3daa5fcae2384
Gudjon Geir Jonsson <gudjongeir@gmail.com>
2014-12-02 17:20:43 +0000

    Started working on how to iterate through all pages in response

--- 500pxMiner/fhpxminer.py
+++ 500pxMiner/fhpxminer.py
@@ -3,4 +3,11 @@ import requests
 
 baseurl = 'https://api.500px.com/v1/photos/search?rpp=100&geo=64.783947%2C-18.235342%2C350km&consumer_key=7mQLRPNEyZHHresRqQVWjfhDViuNniNGJ5iiCV9V'
 response = requests.get(baseurl).json()
-print response['total_items']
+totalPages = response['total_pages']
+photos = response['photos']
+# print type(photos[0])
+for p in photos:
+	print p['id']
+
+# for x in range(1, totalPages + 1):
+# 	print "Page %d of %d" % (x, totalPages)

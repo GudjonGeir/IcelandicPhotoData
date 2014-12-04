784892f0d8e9bb078305f2de40b637f3891e61ff
Gudjon Geir Jonsson <gudjongeir@gmail.com>
2014-12-04 14:23:34 +0000

    Added for loop to get all pages

--- 500pxMiner/fhpxminer.py
+++ 500pxMiner/fhpxminer.py
@@ -8,32 +8,32 @@ baseurl = 'https://api.500px.com/v1/photos/search?rpp=100&geo=64.783947%2C-18.23
 response = requests.get(baseurl).json()
 totalPages = response['total_pages']
 
-photos = response['photos']
 
 
-for p in photos:
-	# Insert camera info if it's not present in table and it's not null
-	if p['camera'] is not None:
-		cursor.execute('''SELECT name FROM Camera WHERE name=?''', (p['camera'],))
+for x in range(1, totalPages + 1):
+	response = requests.get(baseurl + "&page=" + str(x)).json()
+	photos = response['photos']
+	for p in photos:
+		# Insert camera info if it's not present in table and it's not null
+		if p['camera'] is not None:
+			cursor.execute('''SELECT name FROM Camera WHERE name=?''', (p['camera'],))
+			if cursor.fetchone() is None:
+				cursor.execute('INSERT INTO Camera(name) VALUES(?)', (p['camera'],))
+		
+
+		# Insert photohrapher info if not present in table
+		cursor.execute('''SELECT id FROM Photographer WHERE id=?''', (p['user']['id'],))
 		if cursor.fetchone() is None:
-			cursor.execute('INSERT INTO Camera(name) VALUES(?)', (p['camera'],))
-	
+			cursor.execute('INSERT INTO Photographer(id, username, country) VALUES(?, ?, ?)', (p['user']['id'],p['user']['username'],p['user']['country']))
+		
+		# Insert photos
+		cursor.execute('''INSERT INTO Photo(id, Photographer, latitude, longitude, camera, date)
+	                  VALUES(?,?,?,?,?,?)''', (p['id'],p['user']['username'], p['latitude'], p['longitude'],p['camera'],p['taken_at']))
 
-	# Insert photohrapher info if not present in table
-	cursor.execute('''SELECT id FROM Photographer WHERE id=?''', (p['user']['id'],))
-	if cursor.fetchone() is None:
-		cursor.execute('INSERT INTO Photographer(id, username, country) VALUES(?, ?, ?)', (p['user']['id'],p['user']['username'],p['user']['country']))
-	
-	# Insert photos
-	cursor.execute('''INSERT INTO Photo(id, Photographer, latitude, longitude, camera, date)
-                  VALUES(?,?,?,?,?,?)''', (p['id'],p['user']['username'], p['latitude'], p['longitude'],p['camera'],p['taken_at']))
-
+	print "Page %d of %d" % (x, totalPages)
 	
 
 
 db.commit()
 db.close
 print "Finished"
-# for x in range(1, totalPages + 1):
-# 	print "Page %d of %d" % (x, totalPages)
-# text_file.write("Photo name: {0} \n".format(p['name'].encode('utf8')))

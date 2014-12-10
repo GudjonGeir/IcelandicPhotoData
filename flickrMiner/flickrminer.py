980d9e56da970b56073cd9ccd1c85e60d0cfd09c
Gudjon Geir Jonsson <gudjongeir@gmail.com>
2014-12-10 17:47:32 +0000

    Fixed problem with flickr giving the same photos over and over

--- flickrMiner/flickrminer.py
+++ flickrMiner/flickrminer.py
@@ -1,3 +1,6 @@
+import requests
+import sqlite3
+import datetime
 def isNoneOrEmptyOrBlankString (myString):
         if myString:
             if not myString.strip():
@@ -8,37 +11,16 @@ def isNoneOrEmptyOrBlankString (myString):
         return False
 
 
-import requests
-import sqlite3
-db = sqlite3.connect('flickr.sqlite')
-cursor = db.cursor()
-
-errorLog = open("log.txt", "a")
-pageLog = open("pagelog.txt", "a")
-
-
-baseurl = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=e2cc3f278fde020b3a1d4a720893caa9&place_id=jPn7LAVTUb5tyPnsog&media=&per_page=500&format=json&nojsoncallback=1'
-response = requests.get(baseurl).json()
-totalPages = response['photos']['pages']
-
-
-for page in range(10, totalPages + 1):
-	pageLog.write("Page " + str(page) + "\n")
-	response = requests.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=e2cc3f278fde020b3a1d4a720893caa9&place_id=jPn7LAVTUb5tyPnsog&per_page=500&page=" + str(page) + "&format=json&nojsoncallback=1").json()
-	if response['stat'] != "ok":
-		print response
-	photosInPage = response['photos']['photo']
-
+def getPhotoData (page, dbcursor):
 	counter = 0
-
-	for p in photosInPage:
-
+	errorLog = open("log.txt", "a")
+	for p in page:
 		counter += 1
 		if counter % 25 == 0:
-			print "Photo " + str(counter) + " on page " + str(page)
+			print "Photo " + str(counter)
 
 		photoid = p['id']
-		cursor.execute('''SELECT id FROM Photo WHERE id=?''', (photoid,))
+		dbcursor.execute('''SELECT id FROM Photo WHERE id=?''', (photoid,))
 		if cursor.fetchone() is not None:
 			#print "Photo exists, skipping. PhotoID: " + str(photoid)
 			continue
@@ -78,26 +60,62 @@ for page in range(10, totalPages + 1):
 		# Insert camera info if it's not present in table and it's not null
 		if camera is not None: 
 			cursor.execute('''SELECT name FROM Camera WHERE name=?''', (camera,))
-			if cursor.fetchone() is None:
-				cursor.execute('INSERT INTO Camera(name) VALUES(?)', (camera,))
+			if dbcursor.fetchone() is None:
+				dbcursor.execute('INSERT INTO Camera(name) VALUES(?)', (camera,))
 
 
 
 		# # Insert photos
-		cursor.execute('''SELECT id FROM Photo WHERE id=?''', (photoid,))
-		if cursor.fetchone() is None:
-			cursor.execute('''INSERT INTO Photo(id, photographer, latitude, longitude, camera, date)
+		dbcursor.execute('''SELECT id FROM Photo WHERE id=?''', (photoid,))
+		if dbcursor.fetchone() is None:
+			dbcursor.execute('''INSERT INTO Photo(id, photographer, latitude, longitude, camera, date)
 	                  VALUES(?,?,?,?,?,?)''', (photoid, photographer, latitude, longitude, camera, dateTaken))
 
 		# Insert photohrapher info if not present in table
-		cursor.execute('''SELECT photographer FROM Photographer WHERE photographer=?''', (photographer,))
-		if cursor.fetchone() is None:
-			cursor.execute('INSERT INTO Photographer(photographer, country) VALUES(?, ?)', (photographer, ownerLocation))
-	print "Page %d of %d" % (page, totalPages) 
-	db.commit()
+		dbcursor.execute('''SELECT photographer FROM Photographer WHERE photographer=?''', (photographer,))
+		if dbcursor.fetchone() is None:
+			dbcursor.execute('INSERT INTO Photographer(photographer, country) VALUES(?, ?)', (photographer, ownerLocation))
+		errorLog.close()
 
-errorLog.close()
-pagelog.close()
+
+
+
+
+db = sqlite3.connect('flickr.sqlite')
+cursor = db.cursor()
+
+
+maxdate = datetime.date(2014, 12, 10)
+while maxdate > datetime.date(2001, 01, 01):
+	dateLog = open("datelog.txt", "a")
+
+	mindate = maxdate - datetime.timedelta(days=30)
+	baseurl1 = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=e2cc3f278fde020b3a1d4a720893caa9&min_upload_date='
+	baseurl2 = str(mindate) + '&max_upload_date=' + str(maxdate)
+	baseurl3 = '&place_id=jPn7LAVTUb5tyPnsog&per_page=500&format=json&nojsoncallback=1'
+	baseurl4 = '&page='
+
+	response = requests.get(baseurl1 + baseurl2 + baseurl3).json()
+	totalPages = response['photos']['pages']
+
+	for page in range(1, totalPages + 1):
+		response = requests.get(baseurl1 + baseurl2 + baseurl4 + str(page) + baseurl3).json()
+		if response['stat'] != "ok":
+			print response
+		photosInPage = response['photos']['photo']
+
+		getPhotoData(photosInPage, cursor)
+
+		
+			
+		print "Page " + str(page) + " of " + str(totalPages) +  ", " + str(mindate) + "  to  " + str(maxdate)
+		dateLog.write("maxdate = " + str(maxdate) + ", mindate = " + str(mindate))
+		datelog.close()
+		db.commit()
+	
+	maxdate = mindate - datetime.timedelta(days=1)
+
+
 db.close()
 print "Finished"
 
